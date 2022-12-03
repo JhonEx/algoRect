146a4097d1fae50eec1764d016db92f21b6d5923
Jorge-ux <junior.ramirez1306@gmail.com>
2022-12-03 16:55:54 -0500

    containment and adjacency

--- src/main/java/com/example/algorect/app/Junior_Rec/Main.java
+++ src/main/java/com/example/algorect/app/Junior_Rec/Main.java
@@ -6,89 +6,17 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Point topLeftRec1 = new Point(1,4);
-        Point bottomRight1 = new Point(6, 1);
-        Point topLeftRec2 = new Point(7,7);
-        Point bottomRight2 = new Point(10,4);
-
-//        Point topLeftRec1 = new Point(-5,5);
-//        Point bottomRight1 = new Point(-1, 1);
-//        Point topLeftRec2 = new Point(-7,8);
-//        Point bottomRight2 = new Point(-3,5);
-
-//      Second rectangle is smaller than first rectangle
-//        Point topLeftRec2 = new Point(5,4);
-//        Point bottomRight2 = new Point(7,2);
-
-//      Second rectangle is greater than first rectangle
-//        Point topLeftRec2 = new Point(5,6);
-//        Point bottomRight2 = new Point(9,0);
-
-//		CHECK TOP RIGHT
-//		Point topLeftRec2 = new Point(5,9);
-//		Point bottomRight2 = new Point(9,4);
-
-//		CHECK BOTTOM RIGHT
-//		Point topLeftRec2 = new Point(5,0);
-//		Point bottomRight2 = new Point(9,-3);
+        Point topLeftRec1 = new Point(1,5);
+        Point bottomRight1 = new Point(5, 1);
+        Point topLeftRec2 = new Point(1,5);
+        Point bottomRight2 = new Point(5,0);
 
         Rectangles rectangleOne = new Rectangles(topLeftRec1, bottomRight1);
         Rectangles rectangleTwo = new Rectangles(topLeftRec2, bottomRight2);
 
-//		rectangleOne.containment(rectangleTwo);
-//		rectangleOne.intersect(rectangleTwo);
+		rectangleOne.containment(rectangleTwo);
+		rectangleOne.intersect(rectangleTwo);
         rectangleOne.adjacent(rectangleTwo);
 
-
-
-
-
-
-
-//        int bottom1 = 1;
-//        int top1 = 5;
-//        int distancia1 = (int) getDistance(bottom1,top1);
-//        System.out.println("Distancia: " + distancia1);
-//        ArrayList<Integer> lista1 = new ArrayList<>();
-//
-//        for(int i = bottom1; i <= top1; i++){
-//            lista1.add(i);
-//        }
-//
-//        for(int i = 0; i <= distancia1; i++){
-//            System.out.print(lista1.get(i));
-//        }
-//
-//        int bottom2 = 0;
-//        int top2 = 6;
-//        int distancia2 = (int) getDistance(bottom2,top2);
-//        System.out.println("Distancia: " + distancia2);
-//        ArrayList<Integer> lista2 = new ArrayList<>();
-//
-//        for(int i = bottom2; i <= top2; i++){
-//            lista2.add(i);
-//        }
-//
-//        for(int i = 0; i <= distancia2; i++){
-//            System.out.print(lista2.get(i));
-//        }
-//
-//        System.out.println(" * ");
-//
-//        for(int i = 0; i <= distancia1; i++) { //Recorrer Array list del primer rectangulo
-//            for(int j = 0; j <= distancia2; j++){ //Recorrer Array list del segundo rectangulo
-//                if(lista1.get(i) == lista2.get(j)){
-//                    System.out.println("True");
-//                    break;
-//                }else{
-//                    System.out.println("false");
-//                }
-//            }
-//            System.out.println(" - ");
-//        }
-
-    }
-    static double getDistance(double p1, double p2) {
-        return Math.sqrt(Math.pow(p2 - p1, 2));
     }
 }
--- src/main/java/com/example/algorect/app/Junior_Rec/Rectangles.java
+++ src/main/java/com/example/algorect/app/Junior_Rec/Rectangles.java
@@ -6,7 +6,6 @@ import java.util.Arrays;
 
 public class Rectangles {
 
-    // defining 2 main points of a rectangle - bottomRight & topLeft
     private Point bottomRight;
     private Point topLeft;
 
@@ -15,12 +14,11 @@ public class Rectangles {
         this.bottomRight = bottomRight;
     }
 
-    // Detecting if one rectangle contains another
     public boolean containment(Rectangles two) {
         if (this.topLeft.x < two.topLeft.x &
                 this.bottomRight.x > two.bottomRight.x &
-                this.topLeft.y < two.topLeft.y &
-                this.bottomRight.y > two.bottomRight.y) {
+                this.topLeft.y > two.topLeft.y &
+                this.bottomRight.y < two.bottomRight.y) {
             System.out.println("They are contained");
             return true;
         }
@@ -28,8 +26,6 @@ public class Rectangles {
         return false;
     }
 
-    // Detecting intersecting rectangles
-    // Containment counts as an intersection
     public boolean intersect(Rectangles two) {
         if (this.topLeft.y < two.bottomRight.y ||
                 this.bottomRight.y > two.topLeft.y ||
@@ -42,14 +38,11 @@ public class Rectangles {
         return false;
     }
 
-////     Detecting if one rectangle is adjacent to another
 	public boolean adjacent(Rectangles two) {
 
 //       Coordenadas Y
         int dis_height_rec1 = (int) getDistance(this.bottomRight.y, this.topLeft.y);
-        System.out.println("Distancia y_1: " + dis_height_rec1);
         int dis_height_rec2 = (int) getDistance(two.bottomRight.y, two.topLeft.y);
-        System.out.println("Distancia y_2: " + dis_height_rec2);
 
         ArrayList<Integer> vectorY1 = new ArrayList<>();
         for(int i = this.bottomRight.y; i <= this.topLeft.y; i++){
@@ -62,24 +55,15 @@ public class Rectangles {
 
 //       Coordenadas X
         int dis_width_rec1 = (int) getDistance(this.bottomRight.x, this.topLeft.x);
-        System.out.println("Distancia x_1: " + dis_width_rec1);
         int dis_width_rec2 = (int) getDistance(two.bottomRight.x, two.topLeft.x);
-        System.out.println("Distancia x_2: " + dis_width_rec2);
 
         ArrayList<Integer> vectorX1 = new ArrayList<>();
         for(int i = this.topLeft.x; i <= this.bottomRight.x; i++){
             vectorX1.add(i);
         }
-        for(int i = 0; i <= dis_width_rec1; i++){
-            System.out.print(vectorX1.get(i));
-        }
-        System.out.println(" ");
         ArrayList<Integer> vectorX2 = new ArrayList<>();
         for(int i = two.topLeft.x; i <= two.bottomRight.x; i++){
             vectorX2.add(i);
-        }
-        for(int i = 0; i <= dis_width_rec2; i++){
-            System.out.print(vectorX2.get(i));
         }
 
         boolean check1 = check(vectorY1, vectorY2,dis_height_rec1,dis_height_rec2);

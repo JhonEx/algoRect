35994a4b9afa64354aa296fe1da5284c9c43c3ef
jhonr <sir.edwards@gmail.com>
2022-12-04 12:46:23 -0500

    addede intersect test code working good

--- src/test/java/com/example/algorect/app/Junior_Rec/RectanglesTest.java
+++ src/test/java/com/example/algorect/app/Junior_Rec/RectanglesTest.java
@@ -26,12 +26,24 @@ class RectanglesTest {
 
     @Test
     void intersect() {
-        topLeftRec1 = new Point(1,10);
-        bottomRight1 = new Point(8, 1);
+        topLeftRec1 = new Point(1,5);
+        bottomRight1 = new Point(5, 1);
         Rectangles rectangleOne = new Rectangles(topLeftRec1, bottomRight1);
 
-        assertTrue(rectangleOne.intersect(new Rectangles(new Point(2,4), new Point(5,2))));
-        assertFalse(rectangleOne.intersect(new Rectangles(new Point(6,7), new Point(10,4))));
+        //check intersection in axe y.
+        assertTrue(rectangleOne.intersect(new Rectangles(new Point(3,4), new Point(8,2))));
+        assertFalse(rectangleOne.intersect(new Rectangles(new Point(6,4), new Point(8,2))));
+
+        //check intersection in axe x.
+        assertTrue(rectangleOne.intersect(new Rectangles(new Point(2,7), new Point(4,4))));
+        assertFalse(rectangleOne.intersect(new Rectangles(new Point(2,7), new Point(4,6))));
+
+        //check intersection in axe x, y.
+        assertTrue(rectangleOne.intersect(new Rectangles(new Point(3,9), new Point(7,3))));
+        assertFalse(rectangleOne.intersect(new Rectangles(new Point(2,7), new Point(7,6))));
+
+        //check intersection in axe x, y with negatives
+        assertTrue(rectangleOne.intersect(new Rectangles(new Point(-1,8), new Point(3,-1))));
 
     }
 
